79e7261fd8b5c69be41d1ff43580f6175b02db2e
Stephen St. Martin <kuprishuz@gmail.com>
2010-01-30 13:55:18 -0500

    privately scope trigger and remote helpers, to prevent public consumption as they will be removed in favor of a more event based implementation with the release of jQuery 1.4.2 when we can ensure our callbacks are at the end of the chain

--- src/rails.js
+++ src/rails.js
@@ -1,60 +1,62 @@
-jQuery.fn.extend({
-    /**
-     * Triggers a custom event on an element and returns the event result
-     * this is used so that we can catch the result and prevent a later action from occuring
-     * 
-     * TODO: needs a better name
-     */
-    triggerAndReturn: function (name, data) {
-        var event = new jQuery.Event(name);
-        this.trigger(event, data);
+jQuery(function ($) {
+    $.fn.extend({
+        /**
+         * Triggers a custom event on an element and returns the event result
+         * this is used to get around not being able to ensure callbacks are placed
+         * at the end of the chain.
+         *
+         * TODO: deprecate with jQuery 1.4.2 release, in favor of subscribing to our
+         *       own events and placing ourselves at the end of the chain.
+         */
+        triggerAndReturn: function (name, data) {
+            var event = new jQuery.Event(name);
+            this.trigger(event, data);
 
-        return event.result !== false;
-    },
+            return event.result !== false;
+        },
 
-    /**
-     * Handles execution of remote calls firing overridable events along the way
-     *
-     */
-    callRemote: function () {
-        var el      = $(this),
-            data    = [],
-            method  = el.attr('method') || el.attr('data-method') || 'GET',
-            url     = el.attr('action') || el.attr('href') || el.attr('data-url');
+        /**
+         * Handles execution of remote calls firing overridable events along the way
+         *
+         */
+        callRemote: function () {
+            var el      = $(this),
+                data    = [],
+                method  = el.attr('method') || el.attr('data-method') || 'GET',
+                url     = el.attr('action') || el.attr('href') || el.attr('data-url');
 
-        if (el.context.tagName.toUpperCase() === 'FORM') {
-            data = el.serializeArray();
-        }
-
-        // TODO: should let the developer know no url was found
-        if (url !== undefined) {
-            if (el.triggerAndReturn('ajax:before')) {
-                $.ajax({
-                    url: url,
-                    data: data,
-                    type: method.toUpperCase(),
-                    beforeSend: function (xhr) {
-                        xhr.setRequestHeader("Accept", "text/javascript");
-                        el.trigger('ajax:loading', xhr);
-                    },
-                    success: function (data, status, xhr) {
-                        el.trigger('ajax:success', [data, status, xhr]);
-                    },
-                    complete: function (xhr) {
-                        el.trigger('ajax:complete', xhr);
-                    },
-                    error: function (xhr, status, error) {
-                        el.trigger('ajax:failure', [xhr, status, error]);
-                    }
-                });
+            if (el.context.tagName.toUpperCase() === 'FORM') {
+                data = el.serializeArray();
             }
 
-            el.trigger('ajax:after');
-        }
-    }
-});
+            // TODO: should let the developer know no url was found
+            if (url !== undefined) {
+                if (el.triggerAndReturn('ajax:before')) {
+                    $.ajax({
+                        url: url,
+                        data: data,
+                        type: method.toUpperCase(),
+                        beforeSend: function (xhr) {
+                            xhr.setRequestHeader("Accept", "text/javascript");
+                            el.trigger('ajax:loading', xhr);
+                        },
+                        success: function (data, status, xhr) {
+                            el.trigger('ajax:success', [data, status, xhr]);
+                        },
+                        complete: function (xhr) {
+                            el.trigger('ajax:complete', xhr);
+                        },
+                        error: function (xhr, status, error) {
+                            el.trigger('ajax:failure', [xhr, status, error]);
+                        }
+                    });
+                }
 
-jQuery(function ($) {
+                el.trigger('ajax:after');
+            }
+        }
+    });
+
     /**
      *  confirmation handler
      */
@@ -80,3 +82,4 @@ jQuery(function ($) {
         return false;
     });
 });
+$('a').triggerAndReturn('shouldfail');
