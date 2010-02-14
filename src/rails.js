bb1829c50f1b15b1242cf12c1fab7d49ad4f6def
Quin Hoxie <qhoxie@gmail.com>
2010-02-14 15:39:55 +0800

    Narrow scope of disable-with selectors

--- src/rails.js
+++ src/rails.js
@@ -104,8 +104,11 @@ jQuery(function ($) {
     /**
      * disable-with handlers
      */
-    $('form[data-remote]').live('ajax:before', function () {
-        $(this).find('input[data-disable-with]').each(function () {
+    var disable_with_input_selector = 'input[data-disable-with]';
+    var disable_with_form_selector = 'form[data-remote]:has(' + disable_with_input_selector + ')';
+
+    $(disable_with_form_selector).live('ajax:before', function () {
+        $(this).find(disable_with_input_selector).each(function () {
             var input = $(this);
             input.data('enable-with', input.val())
                  .attr('value', input.attr('data-disable-with'))
@@ -113,8 +116,8 @@ jQuery(function ($) {
         });
     });
 
-    $('form[data-remote]').live('ajax:after', function () {
-        $(this).find('input[data-disable-with]').each(function () {
+    $(disable_with_form_selector).live('ajax:after', function () {
+        $(this).find(disable_with_input_selector).each(function () {
             var input = $(this);
             input.removeAttr('disabled')
                  .val(input.data('enable-with'));
