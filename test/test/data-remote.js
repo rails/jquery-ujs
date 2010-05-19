551ad98e6b18c5f9f423591b8e2a903440deca91
Neeraj Singh <neerajdotname@gmail.com>
2010-05-19 21:25:17 +0800

    data-remote test for input tag

--- test/test/data-remote.js
+++ test/test/data-remote.js
@@ -3,13 +3,23 @@
 module('data-remote', {
 	teardown: function() {
 		$('a').remove();
+		$('input').remove();
 	},
 
 	setup: function() {
+
 		$(document.body).append($('<a />', {
 			href: 'http://example.com/address',
 			'data-remote': 'true',
 			text: 'my address'
+		}));
+
+		$(document.body).append($('<input />', {
+			href: 'http://example.com/address',
+			'data-remote': 'true',
+      name: 'submit',
+      type: 'submit',
+      value: 'Click me'
 		}));
 	}
 });
@@ -31,3 +41,20 @@ test('clicking on a link with data-remote attribute', function() {
 	equals(ajaxArgs.type, 'GET', 'ajax arguments should have GET as request type');
 });
 
+
+test('clicking on Submit input tag with data-remote attribute', function() {
+	expect(3);
+
+	var ajaxArgs;
+
+	jack(function() {
+		jack.expect('$.ajax').once().mock(function(args) {
+			ajaxArgs = args;
+		});
+		$('input[data-remote]').trigger('click');
+	});
+
+	equals(ajaxArgs.url, 'http://example.com/address', 'ajax arguments should have passed url');
+	equals(ajaxArgs.dataType, 'script', 'ajax arguments should have script as the data type');
+	equals(ajaxArgs.type, 'GET', 'ajax arguments should have GET as request type');
+});
