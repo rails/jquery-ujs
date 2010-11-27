8b9863b285937c08e333e7ded3876732922fdda0
Neeraj Singh <neerajdotname@gmail.com>
2010-11-27 00:01:36 -0500

    Callback ajax:before is called before any ajax call. jQuery already provides beforeSend callback which jquery-ujs uses to provide ajax:beforeSend callback.
    Anything can be done in ajax:before can also be done
    in ajax:beforeSend callback.
    
    I have been attempting to get the callbacks in
    jquery-ujs close to jQuery and this was the last
    hurdle.

--- test/public/test/call-remote-callbacks.js
+++ test/public/test/call-remote-callbacks.js
@@ -29,14 +29,14 @@ module('call-remote-callbacks', {
 	}
 });
 
-test('if ajax:before callback returns false then do not proceed', function() {
+test('ajax:beforeSend returns false do not proceed', function() {
   expect(0);
   stop();
 
   $('form')
-    .bind('ajax:before', function() { return false; })
-    .bind('ajax:beforeSend', function(){
-      ok(false, 'ajax call should not have been made since ajax:before callback returns false');
+    .bind('ajax:beforeSend', function() { return false; })
+    .bind('ajax:complete', function(){
+      ok(false, 'ajax call should not have been made since ajax:beforeSend callback returns false');
     });
 
   $('form[data-remote]').trigger('submit');
@@ -44,12 +44,11 @@ test('if ajax:before callback returns false then do not proceed', function() {
   App.short_timeout();
 });
 
-test('before, beforeSend, success and complete callbacks should be called', function() {
-	expect(4);
+test('beforeSend, success and complete callbacks should be called', function() {
+	expect(3);
   stop(App.ajax_timeout);
 
 	$('form')
-    .bind('ajax:before', function() { ok(true, 'ajax:before'); return true; })
 	  .bind('ajax:beforeSend',  function(arg) { ok(true, 'ajax:beforeSend'); })
 	  .bind('ajax:success',  function(arg) { ok(true, 'ajax:success'); })
 	  .bind('ajax:complete', function(arg) { ok(true, 'ajax:complete'); start(); });
@@ -57,14 +56,13 @@ test('before, beforeSend, success and complete callbacks should be called', func
 	$('form[data-remote]').trigger('submit');
 });
 
-test('before, beforeSend, error and complete callbacks should be called in case of error', function() {
-	expect(5);
+test('beforeSend, error and complete callbacks should be called in case of error', function() {
+	expect(4);
   $('form').attr('action', App.url('error'));
   stop(App.ajax_timeout);
 
 	$('form')
-    .bind('ajax:before', function() { ok(true, 'ajax:before'); return true; })
-	  .bind('ajax:beforeSend',  function(arg) { ok(true, 'ajax:loading'); })
+	  .bind('ajax:beforeSend',  function(arg) { ok(true, 'ajax:beforeSend'); })
 	  .bind('ajax:error',  function(e, xhr, status, error) { 
       ok(true, 'ajax:error'); 
       equals(xhr.status, 403, 'status code should be 403'); 
