9d77cb79d46fe5f31d3d09654f93a0def8696247
Mislav Marohnić <mislav.marohnic@gmail.com>
2011-01-07 12:14:53 +0100

    whitespace: convert indentation to tabs

--- src/rails.js
+++ src/rails.js
@@ -7,150 +7,146 @@
  *
  */
 
-jQuery(function ($) {
-    var csrf_token = $('meta[name=csrf-token]').attr('content'),
-        csrf_param = $('meta[name=csrf-param]').attr('content');
+jQuery(function($) {
+	var csrf_token = $('meta[name=csrf-token]').attr('content'),
+		csrf_param = $('meta[name=csrf-param]').attr('content');
 
-    $.fn.extend({
-        /**
-         * Triggers a custom event on an element and returns the event result
-         * this is used to get around not being able to ensure callbacks are placed
-         * at the end of the chain.
-         */
-        triggerAndReturn: function (name, data) {
-            var event = new $.Event(name);
-            this.trigger(event, data);
+	$.fn.extend({
+		/**
+		 * Triggers a custom event on an element and returns the event result
+		 * this is used to get around not being able to ensure callbacks are placed
+		 * at the end of the chain.
+		 */
+		triggerAndReturn: function(name, data) {
+			var event = new $.Event(name);
+			this.trigger(event, data);
 
-            return event.result !== false;
-        },
+			return event.result !== false;
+		},
 
-        /**
-         * Handles execution of remote calls. Provides following callbacks:
-         *
-         * - ajax:beforeSend - is executed before firing ajax call
-         * - ajax:success	 - is executed when status is success
-         * - ajax:complete   - is executed when the request finishes, whether in failure or success
-         * - ajax:error      - is execute in case of error
-         */
-        callRemote: function () {
-            var el      = this,
-                method  = el.attr('method') || el.attr('data-method') || 'GET',
-                url     = el.attr('action') || el.attr('href'),
-                dataType  = el.attr('data-type')  || ($.ajaxSettings && $.ajaxSettings.dataType);
+		/**
+		 * Handles execution of remote calls. Provides following callbacks:
+		 *
+		 * - ajax:beforeSend - is executed before firing ajax call
+		 * - ajax:success    - is executed when status is success
+		 * - ajax:complete   - is executed when the request finishes, whether in failure or success
+		 * - ajax:error      - is execute in case of error
+		 */
+		callRemote: function() {
+			var el = this,
+				method = el.attr('method') || el.attr('data-method') || 'GET',
+				url = el.attr('action') || el.attr('href'),
+				dataType = el.attr('data-type') || ($.ajaxSettings && $.ajaxSettings.dataType);
 
-            if (url === undefined) {
-                throw "No URL specified for remote call (action or href must be present).";
-            } else {
-                    var $this = $(this), data = el.is('form') ? el.serializeArray() : [];
+			if (url === undefined) {
+				throw "No URL specified for remote call (action or href must be present).";
+			} else {
+				var $this = $(this), data = el.is('form') ? el.serializeArray() : [];
 
-                    $.ajax({
-                        url: url,
-                        data: data,
-                        dataType: dataType,
-                        type: method.toUpperCase(),
-                        beforeSend: function (xhr) {
-                            if ($this.triggerHandler('ajax:beforeSend') === false) {
-                              return false;
-                            }
-                           // if user has used jQuery.ajaxSetup then call beforeSend callback
-                            var beforeSendGlobalCallback =  $.ajaxSettings && $.ajaxSettings.beforeSend;
-                            if (beforeSendGlobalCallback !== undefined) {
-                                beforeSendGlobalCallback(xhr);
-                            }
-                        },
-                        success: function (data, status, xhr) {
-                            el.trigger('ajax:success', [data, status, xhr]);
-                        },
-                        complete: function (xhr) {
-                            el.trigger('ajax:complete', xhr);
-                        },
-                        error: function (xhr, status, error) {
-                            el.trigger('ajax:error', [xhr, status, error]);
-                        }
-                    });
-            }
-        }
-    });
+				$.ajax({
+					url: url,
+					data: data,
+					dataType: dataType,
+					type: method.toUpperCase(),
+					beforeSend: function(xhr) {
+						if ($this.triggerHandler('ajax:beforeSend') === false) {
+							return false;
+						}
 
-    /**
-     * confirmation handler
-     */
-    $('body').delegate('a[data-confirm], button[data-confirm], input[data-confirm]', 'click.rails', function () {
-        var el = $(this);
-        if (el.triggerAndReturn('confirm')) {
-            if (!confirm(el.attr('data-confirm'))) {
-                return false;
-            }
-        }
-    });
+						// if user has used jQuery.ajaxSetup then call beforeSend callback
+						var beforeSendGlobalCallback = $.ajaxSettings && $.ajaxSettings.beforeSend;
+						if (beforeSendGlobalCallback !== undefined) {
+							beforeSendGlobalCallback(xhr);
+						}
+					},
+					success: function(data, status, xhr) {
+						el.trigger('ajax:success', [data, status, xhr]);
+					},
+					complete: function(xhr) {
+						el.trigger('ajax:complete', xhr);
+					},
+					error: function(xhr, status, error) {
+						el.trigger('ajax:error', [xhr, status, error]);
+					}
+				});
+			}
+		}
+	});
 
+	/**
+	 * confirmation handler
+	 */
+	$('body').delegate('a[data-confirm], button[data-confirm], input[data-confirm]', 'click.rails', function() {
+		var el = $(this);
+		if (el.triggerAndReturn('confirm')) {
+			if (!confirm(el.attr('data-confirm'))) {
+				return false;
+			}
+		}
+	});
 
+	/**
+	 * remote handlers
+	 */
+	$('form[data-remote]').live('submit.rails', function(e) {
+		$(this).callRemote();
+		e.preventDefault();
+	});
 
-    /**
-     * remote handlers
-     */
-    $('form[data-remote]').live('submit.rails', function (e) {
-        $(this).callRemote();
-        e.preventDefault();
-    });
+	$('a[data-remote],input[data-remote]').live('click.rails', function(e) {
+		$(this).callRemote();
+		e.preventDefault();
+	});
 
-    $('a[data-remote],input[data-remote]').live('click.rails', function (e) {
-        $(this).callRemote();
-        e.preventDefault();
-    });
+	/**
+	 * <%= link_to "Delete", user_path(@user), :method => :delete, :confirm => "Are you sure?" %>
+	 *
+	 * <a href="/users/5" data-confirm="Are you sure?" data-method="delete" rel="nofollow">Delete</a>
+	 */
+	$('a[data-method]:not([data-remote])').live('click.rails', function(e) {
+		var link = $(this),
+			href = link.attr('href'),
+			method = link.attr('data-method'),
+			form = $('<form method="post" action="' + href + '"></form>'),
+			metadata_input = '<input name="_method" value="' + method + '" type="hidden" />';
 
-    /**
-     * <%= link_to "Delete", user_path(@user), :method => :delete, :confirm => "Are you sure?" %>
-     *
-     * <a href="/users/5" data-confirm="Are you sure?" data-method="delete" rel="nofollow">Delete</a>
-     */
-    $('a[data-method]:not([data-remote])').live('click.rails', function (e){
-        var link = $(this),
-            href = link.attr('href'),
-            method = link.attr('data-method'),
-            form = $('<form method="post" action="'+href+'"></form>'),
-            metadata_input = '<input name="_method" value="'+method+'" type="hidden" />';
+		if (csrf_param !== undefined && csrf_token !== undefined) {
+			metadata_input += '<input name="' + csrf_param + '" value="' + csrf_token + '" type="hidden" />';
+		}
 
-        if (csrf_param !== undefined && csrf_token !== undefined) {
-            metadata_input += '<input name="'+csrf_param+'" value="'+csrf_token+'" type="hidden" />';
-        }
+		form.hide().append(metadata_input).appendTo('body');
 
-        form.hide()
-            .append(metadata_input)
-            .appendTo('body');
+		e.preventDefault();
+		form.submit();
+	});
 
-        e.preventDefault();
-        form.submit();
-    });
+	/**
+	 * disable-with handlers
+	 */
+	var disable_with_input_selector = 'input[data-disable-with]',
+		disable_with_form_remote_selector = 'form[data-remote]:has(' + disable_with_input_selector + ')',
+		disable_with_form_not_remote_selector = 'form:not([data-remote]):has(' + disable_with_input_selector + ')';
 
-    /**
-     * disable-with handlers
-     */
-    var disable_with_input_selector           = 'input[data-disable-with]',
-        disable_with_form_remote_selector     = 'form[data-remote]:has('       + disable_with_input_selector + ')',
-        disable_with_form_not_remote_selector = 'form:not([data-remote]):has(' + disable_with_input_selector + ')';
+	var disable_with_input_function = function() {
+		$(this).find(disable_with_input_selector).each(function() {
+			var input = $(this);
+			input.data('enable-with', input.val())
+				.attr('value', input.attr('data-disable-with'))
+				.attr('disabled', 'disabled');
+		});
+	};
 
-    var disable_with_input_function = function () {
-        $(this).find(disable_with_input_selector).each(function () {
-            var input = $(this);
-            input.data('enable-with', input.val())
-                .attr('value', input.attr('data-disable-with'))
-                .attr('disabled', 'disabled');
-        });
-    };
+	$(disable_with_form_remote_selector).live('ajax:before.rails', disable_with_input_function);
+	$(disable_with_form_not_remote_selector).live('submit.rails', disable_with_input_function);
 
-    $(disable_with_form_remote_selector).live('ajax:before.rails', disable_with_input_function);
-    $(disable_with_form_not_remote_selector).live('submit.rails', disable_with_input_function);
+	$(disable_with_form_remote_selector).live('ajax:complete.rails', function() {
+		$(this).find(disable_with_input_selector).each(function() {
+			var input = $(this);
+			input.removeAttr('disabled').val(input.data('enable-with'));
+		});
+	});
 
-    $(disable_with_form_remote_selector).live('ajax:complete.rails', function () {
-        $(this).find(disable_with_input_selector).each(function () {
-            var input = $(this);
-            input.removeAttr('disabled')
-                 .val(input.data('enable-with'));
-        });
-    });
-
-    var jqueryVersion = $().jquery;
+	var jqueryVersion = $().jquery;
 
 	if (!( (jqueryVersion === '1.4.3') || (jqueryVersion === '1.4.4'))){
 		alert('This rails.js does not support the jQuery version you are using. Please read documentation.');
