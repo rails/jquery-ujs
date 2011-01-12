993e098167fc3ca66a10b156fd0dab0a52bde069
Mislav Marohnić <mislav.marohnic@gmail.com>
2011-01-12 03:21:51 +0100

    refactor "data-disable-with" tests (relied on implementation too much)

--- test/public/test/data-disable.js
+++ test/public/test/data-disable.js
@@ -1,67 +1,61 @@
 module('data-disable', {
-
-  teardown: App.teardown,
   setup: function() {
-
     $('#qunit-fixture').append($('<form />', {
       action: '/echo',
       'data-remote': 'true',
       method: 'post'
-    }));
-
-    $('form').append($('<input />', {
-      id: 'user_name',
-      'data-disable-with': 'processing ...',
-      type: 'text',
-      size: '30',
-      'name': 'user_name',
-      'value': 'john'
-    }));
+    }))
+      .find('form')
+      .append($('<input type="text" data-disable-with="processing ..." name="user_name" value="john" />'));
 
     $('#qunit-fixture').append($('<form />', {
       action: '/echo',
       method: 'post'
-    }));
-
-    $('form:not([data-remote])').append($('<input />', {
-      id: 'submit',
-      'data-disable-with': 'submitting ...',
-      type: 'submit',
-      name: 'submit',
-      value: 'Submit'
-    }));
-
+    }))
+      .find('form:last')
+      // WEEIRDD: the form won't submit to an iframe if the button is name="submit" (??!)
+      .append($('<input type="submit" data-disable-with="submitting ..." name="submit2" value="Submit" />'));
   }
 });
 
-test('triggering ajax callbacks on a form with data-disable attribute', 6, function() {
+asyncTest('form input field with "data-disable-with" attribute', 7, function() {
   var form = $('form[data-remote]'), input = form.find('input[type=text]');
 
-  ok(!input.is(':disabled'), 'input field should not be disabled');
-  equal(input.val(), 'john', 'input field should have value given to it');
+  function checkOriginalState() {
+    ok(!input.is(':disabled'), 'input field should not be disabled');
+    equal(input.val(), 'john', 'input field should have value given to it');
+  }
+  checkOriginalState();
 
-  form.trigger('ajax:beforeSend');
+  form.bind('ajax:success', function(e, data) {
+    setTimeout(function() {
+      checkOriginalState();
+      equal(data.params.user_name, 'john');
+      start();
+    }, 13)
+  })
+  form.trigger('submit');
 
   ok(input.is(':disabled'), 'input field should be disabled');
   equal(input.val(), 'processing ...', 'input field should have disabled value given to it');
-
-  form.trigger('ajax:complete');
-
-  ok(!input.is(':disabled'), 'input field should not be disabled');
-  equal(input.val(), 'john', 'input field should have value given to it');
 });
 
-test('clicking on non-ajax Submit input tag with data-disable-with attribute', 4, function(){
+asyncTest('form submit button with "data-disable-with" attribute', 7, function(){
   var form = $('form:not([data-remote])'), input = form.find('input[type=submit]');
 
   ok(!input.is(':disabled'), 'input field should not be disabled');
   equal(input.val(), 'Submit', 'input field should have value given to it');
 
-  form.live('submit', function(e) {
-    // prevent the submit navigating away from the test suite
-    e.preventDefault();
+  function checkDisabledState() {
+    ok(input.is(':disabled'), 'input field should be disabled');
+    equal(input.val(), 'submitting ...');
+  }
+
+  form.bind('iframe:loaded', function(e, data) {
+    checkDisabledState();
+    strictEqual(data.params.submit2, undefined);
+    start();
   }).trigger('submit');
 
-  ok(input.is(':disabled'), 'input field should not be disabled');
-  equal(input.val(), 'submitting ...', 'input field should have disabled value given to it');
+  checkDisabledState();
 });
