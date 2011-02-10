c031aef9b21461dc9c3da50766f8fb628236b041
Mislav Marohnić <mislav.marohnic@gmail.com>
2011-02-10 13:38:58 +0100

    correct some "data-confirm" tests
     - don't permanently overwrite window.confirm
     - don't write data() to document.body

--- test/public/test/data-confirm.js
+++ test/public/test/data-confirm.js
@@ -6,14 +6,18 @@ module('data-confirm', {
       'data-confirm': 'Are you absolutely sure?',
       text: 'my social security number'
     }));
+
+    this.windowConfirm = window.confirm;
+  },
+  teardown: function() {
+    window.confirm = this.windowConfirm;
   }
 });
 
 asyncTest('clicking on a link with data-confirm attribute. Confirm yes.', 4, function() {
-  window.confirm = function(msg) {
-    $(document.body).data('confirmation-message', msg);
-    return true;
-  };
+  var message;
+  // auto-confirm:
+  window.confirm = function(msg) { message = msg; return true };
 
   $('a[data-confirm]')
     .bind('ajax:success', function(e, data, status, xhr) { 
@@ -21,34 +25,25 @@ asyncTest('clicking on a link with data-confirm attribute. Confirm yes.', 4, fun
       App.assert_request_path(data, '/echo');
       App.assert_get_request(data); 
 
-      equal( $(document.body).data('confirmation-message'),
-             App.confirmation_message,
-             'confirmation message should be same');
-
+      equal(message, 'Are you absolutely sure?');
       start();
     })
     .trigger('click');
 });
 
 asyncTest('clicking on a link with data-confirm attribute. Confirm No.', 1, function() {
-  window.confirm = function(msg) {
-    $(document.body).data('confirmation-message', msg);
-    return false;
-  };
+  var message;
+  // auto-decline:
+  window.confirm = function(msg) { message = msg; return false };
 
   $('a[data-confirm]')
-    .bind('ajax:before', function(e, data, status, xhr) {
-      App.assert_callback_not_invoked('ajax:before');
+    .bind('ajax:beforeSend', function(e, data, status, xhr) {
+      App.assert_callback_not_invoked('ajax:beforeSend');
     })
     .trigger('click');
 
-  // I don't have idea how to do it without timeout on "confirm: no", will need
-  // to think about that
   setTimeout(function() {
-    equal( $(document.body).data('confirmation-message'),
-            App.confirmation_message,
-            'confirmation message should be same');
-
+    equal(message, 'Are you absolutely sure?');
     start();
-  }, 100);
+  }, 50);
 });
