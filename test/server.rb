85711545c560010dd6af022d9f151cefabd2e316
Steve Schwartz <steve@alfajango.com>
2011-07-15 14:00:54 -0400

    Added googleapis cdn as option in test suite.

--- test/server.rb
+++ test/server.rb
@@ -8,21 +8,33 @@ helpers do
     if params[:version] == version
       "[#{version}]"
     else
-      "<a href='/?version=#{version}'>#{version}</a>"
+      "<a href='/?version=#{version}&cdn=#{params[:cdn]}'>#{version}</a>"
     end
   end
-  
+
+  def cdn_link cdn
+    if params[:cdn] == cdn
+      "[#{cdn}]"
+    else
+      "<a href='/?version=#{params[:version]}&cdn=#{cdn}'>#{cdn}</a>"
+    end
+  end
+
   def jquery_src
-    if params[:version] == 'edge' then "/vendor/jquery.js"
-    else "http://code.jquery.com/jquery-#{params[:version]}.js"
+    if params[:version] == 'edge'
+      "/vendor/jquery.js"
+    elsif params[:cdn] && params[:cdn] == 'googleapis'
+      "https://ajax.googleapis.com/ajax/libs/jquery/#{params[:version]}/jquery.min.js"
+    else
+      "http://code.jquery.com/jquery-#{params[:version]}.js"
     end
   end
-  
+
   def test *names
     names = ["/vendor/qunit.js", "settings"] + names
     names.map { |name| script_tag name }.join("\n")
   end
-  
+
   def script_tag src
     src = "/test/#{src}.js" unless src.index('/')
     %(<script src="#{src}" type="text/javascript"></script>)
@@ -31,13 +43,14 @@ end
 
 get '/' do
   params[:version] ||= '1.6.2'
+  params[:cdn] ||= 'jquery'
   erb :index
 end
 
 [:get, :post, :put, :delete].each do |method|
   send(method, '/echo') {
     data = { :params => params }.update(request.env)
-    
+
     if request.xhr?
       content_type 'application/json'
       data.to_json
