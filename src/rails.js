33ce990ebb45f00813d2bee9658ac244ba1bc879
Steve Schwartz <steve@alfajango.com>
2012-08-13 13:34:47 -0400

    Modified form button click selector. Fixes #63.

--- src/rails.js
+++ src/rails.js
@@ -59,7 +59,7 @@
     formSubmitSelector: 'form',
 
     // Form input elements bound by jquery-ujs
-    formInputClickSelector: 'form input[type=submit], form input[type=image], form button[type=submit], form button:not(button[type])',
+    formInputClickSelector: 'form input[type=submit], form input[type=image], form button[type=submit], form button:not([type])',
 
     // Form input elements disabled during form submission
     disableSelector: 'input[data-disable-with], button[data-disable-with], textarea[data-disable-with]',
