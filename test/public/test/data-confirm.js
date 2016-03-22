a5beaae06e4a28b98b92d51ae7133b67be582209
ZhangRGK <stamen0630@gmail.com>
2016-03-22 10:07:36 +0800

    FIX:修复 confirm 两处测试

--- test/public/test/data-confirm.js
+++ test/public/test/data-confirm.js
@@ -135,34 +135,49 @@ asyncTest('4. clicking on a button with data-confirm attribute. Confirm No.', 3,
   }, 50);
 });
 
-//TODO
+asyncTest('5. clicking on a button with data-confirm attribute. Confirm error.', 3, function() {
+  var message;
+  var originConfirm = $.rails.confirm
+  $.rails.confirm = function(m) {
+    return new Promise(function(resolve, reject) {
+      try {
+        var dialog = simple.dialog.confirm({
+          content: m,
+          callback: function(e, yes) {
+            resolve(yes);
+          }
+        });
+        throw 'random exception';
+      } catch(e) {
+        resolve(false);
+      }
+    });
+  }
 
-// asyncTest('5. clicking on a button with data-confirm attribute. Confirm error.', 3, function() {
-//   var message;
-//   // auto-decline:
-//   window.confirm = function(msg) { message = msg; throw "some random error"; };
+  $('button[data-confirm]')
+    .bind('confirm:complete', function(e, data) {
+      App.assertCallbackInvoked('confirm:complete');
+      ok(data == false, 'confirm:complete passes in confirm answer (false)');
+    })
+    .bind('ajax:beforeSend', function(e, data, status, xhr) {
+      App.assertCallbackNotInvoked('ajax:beforeSend');
+    })
+    .trigger('click');
 
-//   $('button[data-confirm]')
-//     .bind('confirm:complete', function(e, data) {
-//       App.assertCallbackInvoked('confirm:complete');
-//       ok(data == false, 'confirm:complete passes in confirm answer (false)');
-//     })
-//     .bind('ajax:beforeSend', function(e, data, status, xhr) {
-//       App.assertCallbackNotInvoked('ajax:beforeSend');
-//     })
-//     .trigger('click');
+  $simpleDialog = $('.simple-dialog-confirm');
+  message = $simpleDialog.find('.simple-dialog-content').text();
 
-//   setTimeout(function() {
-//     equal(message, 'Are you absolutely sure?');
-//     start();
-//   }, 50);
-// });
+  setTimeout(function() {
+    equal(message, 'Are you absolutely sure?');
+    start();
+
+    $.rails.confirm = originConfirm
+  }, 50);
+});
 
 asyncTest('6. clicking on a submit button with form and data-confirm attributes. Confirm No.', 3, function() {
   var message;
   var $simpleDialog;
-  // auto-decline:
-  window.confirm = function(msg) { message = msg; return false };
 
   $('input[type=submit][form]')
     .bind('confirm:complete', function(e, data) {
@@ -274,19 +289,33 @@ asyncTest('10. binding to confirm:complete event of a button and returning false
   }, 50);
 });
 
-//TODO
+asyncTest('11. a button inside a form only confirms once', 1, function() {
+  var confirmations = 0;
 
-// asyncTest('11. a button inside a form only confirms once', 1, function() {
-//   var confirmations = 0;
+  $.rails.confirm = function(m) {
+    confirmations += 1;
+    return new Promise(function(resolve, reject) {
+      try {
+        var dialog = simple.dialog.confirm({
+          content: m,
+          callback: function(e, yes) {
+            resolve(yes);
+          }
+        });
+      } catch(e) {
+        resolve(false);
+      }
+    });
+  }
 
-//   $('#qunit-fixture').append($('<form />').append($('<button />', {
-//     'data-remote': 'true',
-//     'data-confirm': 'Are you absolutely sure?',
-//     text: 'Click me'
-//   })));
+  $('#qunit-fixture').append($('<form />').append($('<button />', {
+    'data-remote': 'true',
+    'data-confirm': 'Are you absolutely sure?',
+    text: 'Click me'
+  })));
 
-//   $('form > button[data-confirm]').trigger('click');
+  $('form > button[data-confirm]').trigger('click');
 
-//   ok(confirmations === 1, 'confirmation counter should be 1, but it was ' + confirmations);
-//   start();
-// });
+  ok(confirmations === 1, 'confirmation counter should be 1, but it was ' + confirmations);
+  start();
+});
