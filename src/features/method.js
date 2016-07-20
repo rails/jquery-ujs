ecc555b9993f645cf07972882dcb353f841fd6cc
Dangyi Liu <leedy.liu@gmail.com>
2016-07-20 11:15:26 +0800

    features/method: refactoring for less indents

--- src/features/method.js
+++ src/features/method.js
@@ -5,23 +5,25 @@ import * as ajax from '../utils/ajax'
 // <a href="/users/5" data-method="delete" rel="nofollow" data-confirm="Are you sure?">Delete</a>
 export function handleMethod(e) {
   var link = $(e.target)
-  if (link.data('method')) {
-    var href = ajax.href(link),
-        method = link.data('method'),
-        target = link.attr('target'),
-        csrfToken = csrf.csrfToken(),
-        csrfParam = csrf.csrfParam(),
-        form = $('<form method="post" action="' + href + '"></form>'),
-        metadataInput = '<input name="_method" value="' + method + '" type="hidden" />'
 
-    if (csrfParam !== undefined && csrfToken !== undefined && !ajax.isCrossDomain(href)) {
-      metadataInput += '<input name="' + csrfParam + '" value="' + csrfToken + '" type="hidden" />'
-    }
+  if (!link.data('method')) return
 
-    if (target) { form.attr('target', target) }
+  var href = ajax.href(link),
+      method = link.data('method'),
+      target = link.attr('target'),
+      csrfToken = csrf.csrfToken(),
+      csrfParam = csrf.csrfParam(),
+      form = $('<form method="post" action="' + href + '"></form>'),
+      metadataInput = '<input name="_method" value="' + method + '" type="hidden" />'
 
-    form.hide().append(metadataInput).appendTo('body')
-    form.submit()
+  if (csrfParam !== undefined && csrfToken !== undefined && !ajax.isCrossDomain(href)) {
+    metadataInput += '<input name="' + csrfParam + '" value="' + csrfToken + '" type="hidden" />'
   }
+
+  if (target) { form.attr('target', target) }
+
+  form.hide().append(metadataInput).appendTo('body')
+  form.submit()
+
   return false
 }
