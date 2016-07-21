6093ec154e95c1243d0adc6b84e9ecce0d605ed9
Dangyi Liu <leedy.liu@gmail.com>
2016-07-21 15:12:41 +0800

    Refine utils/csrf

--- src/utils/csrf.js
+++ src/utils/csrf.js
@@ -1,20 +1,26 @@
 // Up-to-date Cross-Site Request Forgery token
 export function csrfToken() {
-  return $('meta[name=csrf-token]').attr('content')
+  let meta = document.querySelector('meta[name=csrf-token]')
+  return meta && meta.content
 }
 
 // URL param that must contain the CSRF token
 export function csrfParam() {
-  return $('meta[name=csrf-param]').attr('content')
+  let meta = document.querySelector('meta[name=csrf-param]')
+  return meta && meta.content
 }
 
 // Make sure that every Ajax request sends the CSRF token
 export function CSRFProtection(xhr) {
-  var token = csrfToken()
+  let token = csrfToken()
   if (token) xhr.setRequestHeader('X-CSRF-Token', token)
 }
 
 // Make sure that all forms have actual up-to-date tokens (cached forms contain old ones)
 export function refreshCSRFTokens() {
-  $('form input[name="' + csrfParam() + '"]').val(csrfToken())
+  let token = csrfToken(),
+      inputs = document.querySelectorAll('form input[name="' + csrfParam() + '"]')
+  Array.prototype.forEach.call(inputs, input => {
+    input.value = token
+  })
 }
